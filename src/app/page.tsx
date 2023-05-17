a3747d6db87f93dd1bc4ae3cb9f8daf1c406e03f
Domogo <domsatrapa@gmail.com>
2023-05-17 21:52:28 +0200

    add links to examples repo and youtube video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,35 @@ import { ImageGallery } from "@/components/ImageGallery";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-4">
-      <div>
+      <div className="cursor-default">
         <h1 className="text-2xl font-bold">
           Gallery for the Creative Programming Workshop
         </h1>
         <p>
           This is a gallery of creative coding projects made by TCP members in
           the Creative Programming workshop.
+        </p>
+        <p>
+          The workshop source code can be found{" "}
+          <a
+            className="text-indigo-500 cursor-pointer hover:underline"
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://github.com/Tech-City-Pula/tcp-kreativno-programiranje"
+          >
+            here
+          </a>
+        </p>
+        <p>
+          Altough in croatian, the video of the workshop can be found on{" "}
+          <a
+            className="text-indigo-500 cursor-pointer hover:underline"
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://www.youtube.com/watch?v=TAQQuqptXMw"
+          >
+            YouTube
+          </a>
         </p>
       </div>
 
